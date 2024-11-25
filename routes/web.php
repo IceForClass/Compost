09c1987ebb4b703874037d73793dd8c0206832d2
Icesito <146034810+IceForClass@users.noreply.github.com>
2024-11-25 18:26:31 +0000

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -17,4 +17,8 @@ Route::middleware('auth')->group(function () {
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
+Route::get('/prueba', function () {
+    return "El despligue en el vps funciona correctamente";
+});
+
 require __DIR__.'/auth.php';
