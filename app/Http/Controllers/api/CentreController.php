be3224a844b76b55a35ebd91b5842e7b70df3439
Icesito68 <113939920+Icesito68@users.noreply.github.com>
2024-11-27 08:34:30 +0000

    Add: api query center

--- app/Http/Controllers/api/CentreController.php
+++ app/Http/Controllers/api/CentreController.php
@@ -3,10 +3,14 @@
 namespace App\Http\Controllers\Api;
 
 use App\Models\Centre;
+use Orion\Concerns\DisableAuthorization;
+use Orion\Concerns\DisablePagination;
 use Orion\Http\Controllers\Controller;
 
 class CentreController extends Controller
 {
-    protected $model = Centre::class; // or "App\Models\Post"
+    use DisableAuthorization;
+    use DisablePagination;
+    protected $model = Centre::class;
 
 }
