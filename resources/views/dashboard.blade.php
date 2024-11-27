e9e9c71426b59252162f7525c619f202f9863829
Icesito <113939920+Icesito68@users.noreply.github.com>
2024-11-27 17:26:27 +0000

    Fix: Idk what happend with all

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -13,6 +13,7 @@
                 <div class="p-6 text-gray-900 dark:text-gray-100">
                     {{ __("You're logged in!") }}
                 </div>
+                <x-select-composter />
                 <div>
                     <button id="mostrarDatos">
                         Consulta de institutos
