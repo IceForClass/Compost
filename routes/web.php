225a1e63a36c6412197fa640414931632072a0ea
Icesito <113939920+Icesito68@users.noreply.github.com>
2024-12-03 20:33:39 +0000

    Add: Initial email support

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\MailController;
 use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
@@ -17,5 +18,7 @@ Route::middleware('auth')->group(function () {
 
 Route::resource('users', UserController::class)->middleware('can:administrate,App\Models\User')
 ->middleware('can:administrate,App\Models\User');
+
+Route::get('send-mail', [MailController::class, 'index']);
 
 require __DIR__.'/auth.php';
