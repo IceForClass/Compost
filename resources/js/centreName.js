2391621c9313c7dd540fd74ad8112be22720cbec
Icesito68 <113939920+Icesito68@users.noreply.github.com>
2024-12-05 10:59:13 +0000

    Finish: Get center name with user id

--- resources/js/centreName.js
+++ resources/js/centreName.js
@@ -1,7 +1,8 @@
 import { fetchData } from "./api.js";
 
 export function centreName() {
-    fetchData("/api/users/1/centres/").then((data) => {
+    const idUser = sessionStorage.getItem("idUser");
+    fetchData(`/api/users/${idUser}/centres/`).then((data) => {
         const centreData = data.data[0];
         const container = document.getElementById("nombreCentro");
 
